9951656e35754b40bc33f38b188abd790dfba78b
AndreyDonets <donec2506@gmail.com>
2020-08-26 21:13:54 +0300

    Добавил проверку на налицие рецепта и ингридиентов

--- task2/AddIngredient.cs
+++ task2/AddIngredient.cs
@@ -11,7 +11,10 @@ namespace task2
             CreateIngredient ingredient = new CreateIngredient();
             Console.WriteLine("Введите название ингридиента");
             ingredient.Name = Console.ReadLine();
-            ingredient.Create(ingredients);
+            if (!ingredients.Exists(x => x.Name == ingredient.Name))
+                ingredient.Create(ingredients);
+            else
+                Console.WriteLine("Такой ингридиент уже есть в списке, нажмите что бы продолжить.");
         }
     }
 }
--- task2/CreateRecipe.cs
+++ task2/CreateRecipe.cs
@@ -8,17 +8,35 @@ namespace task2
     {
         public static void Create(List<modelSubdirectory> recipes, string path)
         {
-            CreateRecipeFinish recipe = new CreateRecipeFinish();
-            Console.WriteLine("Придумайте название рецепта");
-            recipe.Name = Console.ReadLine();
-            Console.WriteLine("Опишите рецепт");
-            recipe.Description = Console.ReadLine();
-            recipe.Ingredients = ChooseIngredient.Choose();
-            recipe.Cooking = CookingSteps.Cook();
-            ChangeSubdirectory changeSubdirectory = new ChangeSubdirectory();
-            recipe.Create(recipe);
-            changeSubdirectory.ChangeSub(recipes, recipe.Name, path);
-            Console.Clear();
+            Console.WriteLine("Придумайте название рецепта, exit для выхода");
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "exit")
+                {
+                    break;
+                }
+                if (!recipes.Exists(x => x.Name == name))
+                {
+                    CreateRecipeFinish recipe = new CreateRecipeFinish();
+                    Console.WriteLine("Опишите рецепт");
+                    recipe.Name = name;
+                    recipe.Description = Console.ReadLine();
+                    recipe.Ingredients = ChooseIngredient.Choose();
+                    recipe.Cooking = CookingSteps.Cook();
+                    ChangeSubdirectory changeSubdirectory = new ChangeSubdirectory();
+                    recipe.Create(recipe);
+                    changeSubdirectory.ChangeSub(recipes, recipe.Name, path);
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Такой рецепт уже есть, придумайте другой, exit для выхода");
+                }
+            }
         }
     }
 }
